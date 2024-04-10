ddc80b73b13dd697fb1c11d7acd224fbe62140ae
shooding <shooding@gmail.com>
2024-04-10 20:30:29 +0800

    first release

---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+
+namespace MyDriveProject
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Check for folder ID argument
+            if (args.Length != 4)
+            {
+                Console.WriteLine("Usage: dotnet run <label-id> <field-id> <selection-id> <folder-id>");
+                return;
+            }
+
+            string labelId = args[0];
+            string fieldId = args[1];
+            string selectionId = args[2];
+            string folderId = args[3];            
+
+            // Load credentials
+            var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                GoogleClientSecrets.FromFile("./credentials.json").Secrets,
+                [DriveService.Scope.Drive],
+                "user",
+                CancellationToken.None
+            );
+
+            // Create Drive service
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "My Drive Project",
+            });
+
+            var allFiles = ListFilesRecursively(service, folderId, labelId); // Start with given folderId and labelId         
+            Console.WriteLine("allFiles.Count=" + allFiles.Count);
+            var numProcessed = 0;
+
+            // 50,000 requests per project per day, which can be increased. 10 queries per second (QPS) per IP address.
+            foreach (var file in allFiles)
+            {
+                numProcessed++;
+                Console.WriteLine("File Name: " + file.Name);
+                Console.WriteLine("File ID: " + file.Id);
+                Console.WriteLine("----------------------");
+
+                // 套用既有標籤到檔案
+                // Create the ModifyLabelsRequest object
+                var modifyRequest = new ModifyLabelsRequest();
+
+                // Apply label See https://stackoverflow.com/questions/76426947/setting-new-drive-labels-with-selections-to-a-specific-file-using-google-apps-sc
+                var addModification = new LabelModification
+                {
+                    LabelId = labelId,
+                    FieldModifications = [
+                        new LabelFieldModification
+                        {
+                            FieldId = fieldId,
+                            SetSelectionValues = [selectionId] // 值必須是 selectionId 請先從 label.get API +  得到
+                        }
+                    ]
+                };
+                modifyRequest.LabelModifications = [addModification];
+
+                try
+                {
+                    // Call the ModifyLabels method
+                    await service.Files.ModifyLabels(modifyRequest, file.Id).ExecuteAsync();
+
+                    Console.WriteLine("Labels updated successfully! " + (numProcessed * 100 / allFiles.Count) + "% done");
+
+                    System.Threading.Thread.Sleep(250);
+                }
+                catch (System.Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        static List<Google.Apis.Drive.v3.Data.File> ListFilesRecursively(DriveService service, string parentFolderId, string labelId)
+        {
+            var results = new List<Google.Apis.Drive.v3.Data.File>();
+            var request = service.Files.List();
+            request.Q = $"'{parentFolderId}' in parents";  // Modify the 'q' query if needed
+            request.Fields = "nextPageToken, files(id, name, mimeType)"; // 需要加入查詢mimeType 才能用做判斷是否為目錄
+            request.IncludeItemsFromAllDrives = true; // Include files from Shared Drives
+            request.SupportsAllDrives = true;         // Required if including files from Shared Drives             
+
+            do
+            {
+                var fileList = request.Execute();
+
+                foreach (var file in fileList.Files)
+                {
+                    if (file.MimeType == "application/vnd.google-apps.folder")
+                    {
+                        // Folders
+                        results.AddRange(ListFilesRecursively(service, file.Id, labelId));
+                    }
+                    else
+                    {
+                        // Files
+
+                        // try
+                        // {
+                        //     var listRequest = service.Files.ListLabels(file.Id);
+                        //     var labels = await listRequest.ExecuteAsync();
+
+                        //     foreach (var label in labels.Labels)
+                        //     {
+
+                        //         if (label.Id == labelId){
+                        //             Console.WriteLine("File Name: "+file.Name+" already labeled.");
+                        //             // Already labeled
+                        //             continue;
+                        //         }
+                        //     }                            
+                        // }
+                        // catch (Exception ex)
+                        // {
+                        //     Console.WriteLine($"Error getting labels: {ex.Message}");
+                        // }
+
+                        // Not labeled yet
+                        results.Add(file);
+                    }
+                }
+
+                request.PageToken = fileList.NextPageToken;
+            } while (!String.IsNullOrEmpty(request.PageToken));
+
+            return results;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,99 @@
+# Purpose
+
+- A tool for Google Workspace Manager to apply labels recursively to any specified folder.
+- Google Drive folder is not an "actual" folder, to apply labels like "Classified" or "Restricted" on files you need to navigate into each folder and select manually 100 files at most.
+- This tool can save you lots of time if you're also going through ISO27001 verification process.
+
+- The **"Classified", "Restricted"** labels you created from Google Drive UI is actually via Google Label API v2. In order to show highlighted **"Classified"** as ISO27001 requested, you need to specify selection id of the label.
+
+- You can find out LabelID, FieldID and SelectionID from here [v2 label API](https://developers.google.com/drive/labels/reference/rest/v2/labels/list) opened via browser and login with workspace account. Click on "Try it!"
+![](images/tryit.png)
+
+- The tricky part is select view = **"LABEL_VIEW_FULL"** in order to see all attributes.
+![](images/labelviewfull.png)
+- Then you get responses like including LabelID, FieldID and SelectionID:
+
+```json
+{
+  "name": "labels/s6O**************Fcb@22",
+  "id": "s6O**************Fcb",
+  "revisionId": "22",
+  "labelType": "ADMIN",
+  "creator": {
+    "person": "people/10*******77"
+  },
+  "createTime": "2023-12-04T01:56:59.455162Z",
+  "revisionCreator": {
+    "person": "people/10*******77"
+  },
+  "revisionCreateTime": "2024-03-28T02:31:15.952314Z",
+  "publisher": {
+    "person": "people/10*******77"
+  },
+  "publishTime": "2024-03-28T02:31:15.952314Z",
+  "customer": "customers/C*******h",
+  "properties": {
+    "title": "Data classification",
+    "description": "According to ISO Data classification process, documents should have correct labels."
+  },
+  "lifecycle": {
+    "state": "PUBLISHED"
+  },
+  ...  
+  "fields": [
+    {
+      "id": "<field-id>",
+      "queryKey": "labels/s6O**************Fcb",
+      "properties": {
+        "displayName": "Classification level"
+      },
+      ...
+      "selectionOptions": {
+        "choices": [
+          {
+            "id": "<selection-id-1>",
+            "properties": {
+              "displayName": "Classified",
+              "description": "Including source code, patents, business plans, bank accounts, transaction data etc.",
+              ... }        
+          },
+          {
+            "id": "<selection-id-2>",
+            "properties": {
+              "displayName": "Restricted",
+              "description": "Only for selected role. Including Project documents, Customer data, contracts, resume, payroll and documents including personal information.",
+              ... }
+          },
+          {
+            "id": "<selection-id-3>",
+            "properties": {
+              "displayName": "NORMAL",
+              "description": "Process Documents, Product Categories, etc.",
+              ... }
+          }
+      	  ]
+      }
+    }
+  ]
+}
+```
+
+# Setup
+
+- To obtain your **credentials.json**, go to `https://console.cloud.google.com/apis/credentials` and create **"OAuth Client ID"**. We're not publishing the application so consent screen is not required.
+- Enable Google Drive API and Drive Labels API.
+- Download secret and rename to **credentials.json** 
+- Need **.NET** to run this code with latest Google Drive API v3 and Google Label API v2. 
+- For macOS, download `https://dotnet.microsoft.com` LTS version e.g. 8.0
+Choose macOS arm64 or x86 depends on your machine.
+
+# Run
+
+- FolderID is the the last part of Google Drive url `https://drive.google.com/drive/u/0/folders/<folder-id>`. Then you can run the code, login with workspace account and allow access. Don't worry, the application is created by you (**"OAuth Client ID"**) not me.
+- The code works as recursively go through folders and keep a list of files to label, then slowly call modify label API to avoid rate limit of API. Free to sleep less for acceleration. 
+
+```bash
+dotnet run <label-id> <field-id> <selection-id> <folder-id>
+```
+
+![](images/running.png)
